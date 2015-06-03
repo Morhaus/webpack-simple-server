50bdb7e98e7dd103fc65b0069dc1cde817df90f3
Morhaus <alexandre.kirszenberg@gmail.com>
2015-06-03 22:57:36 +0200

    Fix cli

--- src/cli.js
+++ src/cli.js
@@ -11,7 +11,7 @@ let configPath = path.resolve(
 let hot = argv.h || argv.hot;
 let directory = path.resolve(
   './',
-  argv._ || argv.d || argv.directory
+  argv._[0] || argv.d || argv.directory
 );
 let port = argv.p || argv.port;
 let devServerPort = argv.devServerPort;
